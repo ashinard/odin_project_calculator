58b5720e19ed1d5d2a2a56ac1c401b80eabea17e
Anthony Shinard <a.shinard12@gmail.com>
2024-06-20 09:32:55 -0700

    Fix pressed function and Calculator constructor

--- calculator.js
+++ calculator.js
@@ -7,11 +7,11 @@ function Calculator(){
     };
 
     this.calculate = function(str){
-        let sep = str.splice(' ');
+        let sep = str.split(' ');
 
-        a = sep[0];
+        a = parseInt(sep[0]);
         op = sep[1];
-        b = sep[2];
+        b = parseInt(sep[2]);
 
         if(!this.math[op] || isNaN(a) || isNaN(b))
             {
@@ -28,12 +28,15 @@ function pressed(e){
     }
     else if(e.target.innerHTML === "+" || e.target.innerHTML === "-" 
     || e.target.innerHTML === "*" || e.target.innerHTML === "/"){
-        if(display.value.indexOf(e.target.innerHTML) === -1){
+        if(display.value.indexOf('+') === -1 && display.value.indexOf('-') === -1 &&
+           display.value.indexOf('*') === -1 && display.value.indexOf('/') === -1){
             display.value += " " + e.target.innerHTML + " ";
         }
     }
     else if(e.target.name === "equals"){
-        display.value = math.calculate(display.value);
+        let result = math.calculate(display.value);
+        console.log(result);
+        display.value = result;
     }
     else{
         display.value += e.target.innerHTML;
